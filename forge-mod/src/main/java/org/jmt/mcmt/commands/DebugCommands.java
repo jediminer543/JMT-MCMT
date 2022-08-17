a133a124351a11c7bf6cd6da8d4287e250d3e857
jediminer543 <jediminer543@users.noreply.github.com>
2022-08-17 11:23:34 +0100

    Re-add tracking to debug command

--- forge-mod/src/main/java/org/jmt/mcmt/commands/DebugCommands.java
+++ forge-mod/src/main/java/org/jmt/mcmt/commands/DebugCommands.java
@@ -9,16 +9,20 @@ import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.ResourceOrTagLocationArgument;
+import net.minecraft.commands.arguments.ResourceOrTagLocationArgument.Result;
 import net.minecraft.commands.arguments.coordinates.Coordinates;
 import net.minecraft.commands.arguments.coordinates.Vec3Argument;
 import net.minecraft.core.BlockPos;
@@ -29,8 +33,11 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.commands.LocateCommand;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.TickingBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -38,7 +45,8 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
 
 public class DebugCommands {
 
-	public static LiteralArgumentBuilder<CommandSourceStack> registerDebug(LiteralArgumentBuilder<CommandSourceStack> root) {
+	public static LiteralArgumentBuilder<CommandSourceStack> registerDebug(
+			LiteralArgumentBuilder<CommandSourceStack> root) {
 		return root.then(Commands.literal("getBlockState")
 				.then(Commands.argument("location", Vec3Argument.vec3()).executes(cmdCtx -> {
 					Coordinates loc = Vec3Argument.getCoordinates(cmdCtx, "location");
@@ -68,9 +76,10 @@ public class DebugCommands {
 									"Block at " + bp + " is " + bs.getBlock().getRegistryName() + " with TE NBT:");
 							cmdCtx.getSource().sendSuccess(message, true);
 							cmdCtx.getSource().sendSuccess(itc, true);
-							//System.out.println(message.toString());
+							// System.out.println(message.toString());
 							return 1;
-				}))).then(Commands.literal("tick").requires(cmdSrc -> {
+						})))
+				.then(Commands.literal("tick").requires(cmdSrc -> {
 					return cmdSrc.hasPermission(2);
 				}).then(Commands.literal("te"))
 						.then(Commands.argument("location", Vec3Argument.vec3()).executes(cmdCtx -> {
@@ -100,37 +109,40 @@ public class DebugCommands {
 					}
 					// Copypasta from syncfu;
 					Arrays.stream(System.getProperty("java.class.path").split(File.pathSeparator)).flatMap(path -> {
-			        	File file = new File(path);
-			        	if (file.isDirectory()) {
-			        		return Arrays.stream(file.list((d, n) -> n.endsWith(".jar")));
-			        	}
-			        	return Arrays.stream(new String[] {path});
-			        }).filter(s -> s.endsWith(".jar"))
-					.map(Paths::get).forEach(path -> {
-			        	Path name = path.getFileName();
-			        	try {
-							Files.copy(path, Paths.get(base.toString(), name.toString()), StandardCopyOption.REPLACE_EXISTING);
+						File file = new File(path);
+						if (file.isDirectory()) {
+							return Arrays.stream(file.list((d, n) -> n.endsWith(".jar")));
+						}
+						return Arrays.stream(new String[] { path });
+					}).filter(s -> s.endsWith(".jar")).map(Paths::get).forEach(path -> {
+						Path name = path.getFileName();
+						try {
+							Files.copy(path, Paths.get(base.toString(), name.toString()),
+									StandardCopyOption.REPLACE_EXISTING);
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
 					});
-					
-					
-					TextComponent message = new TextComponent("Classpath Dumped to: " + base.toAbsolutePath().toString());
+
+					TextComponent message = new TextComponent(
+							"Classpath Dumped to: " + base.toAbsolutePath().toString());
 					cmdCtx.getSource().sendSuccess(message, true);
 					System.out.println(message.toString());
 					return 1;
 				}))
-				/* 1.16.1 code; AKA the only thing that changed  */
+				/* 1.16.1 code; AKA the only thing that changed */
 				.then(Commands.literal("test").requires(cmdSrc -> {
 					return cmdSrc.hasPermission(2);
 				}).then(Commands.literal("structures").executes(cmdCtx -> {
 					ServerPlayer p = cmdCtx.getSource().getPlayerOrException();
 					BlockPos srcpos = p.blockPosition();
 					UUID id = p.getUUID();
-					int index = structureIdx.computeIfAbsent(id.toString(), (s) -> new AtomicInteger()).getAndIncrement();
-					Registry<ConfiguredStructureFeature<?, ?>> registry = cmdCtx.getSource().getLevel().registryAccess().registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY);
-					List<Holder.Reference<ConfiguredStructureFeature<?, ?>>> targets = registry.holders().collect(Collectors.toUnmodifiableList());
+					int index = structureIdx.computeIfAbsent(id.toString(), (s) -> new AtomicInteger())
+							.getAndIncrement();
+					Registry<ConfiguredStructureFeature<?, ?>> registry = cmdCtx.getSource().getLevel().registryAccess()
+							.registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY);
+					List<Holder.Reference<ConfiguredStructureFeature<?, ?>>> targets = registry.holders()
+							.collect(Collectors.toUnmodifiableList());
 					Holder.Reference<ConfiguredStructureFeature<?, ?>> target = null;
 					if (index >= targets.size()) {
 						target = targets.get(0);
@@ -138,30 +150,53 @@ public class DebugCommands {
 					} else {
 						target = targets.get(index);
 					}
-					Pair<BlockPos, Holder<ConfiguredStructureFeature<?, ?>>> dst = cmdCtx.getSource().getLevel().getChunkSource().getGenerator().findNearestMapFeature(cmdCtx.getSource().getLevel(), HolderSet.direct(target), srcpos, 100, false);
+					Pair<BlockPos, Holder<ConfiguredStructureFeature<?, ?>>> dst = cmdCtx.getSource().getLevel()
+							.getChunkSource().getGenerator().findNearestMapFeature(cmdCtx.getSource().getLevel(),
+									HolderSet.direct(target), srcpos, 100, false);
 					if (dst == null) {
-						TextComponent message = new TextComponent("Failed locating " + target.key().location().toString() + " from " + srcpos);
+						TextComponent message = new TextComponent(
+								"Failed locating " + target.key().location().toString() + " from " + srcpos);
 						cmdCtx.getSource().sendSuccess(message, true);
 						return 1;
 					}
 					TextComponent message = new TextComponent("Found target; loading now");
 					cmdCtx.getSource().sendSuccess(message, true);
-					p.teleportTo(dst.getFirst().getX(), srcpos.getY(), dst.getFirst().getZ()); 
-					//LocateCommand.showLocateResult(cmdCtx.getSource(), ResourceOrTagLocationArgument.getStructureFeature(p_207508_, "structure"), srcpos, dst, "commands.locate.success");
+					p.teleportTo(dst.getFirst().getX(), srcpos.getY(), dst.getFirst().getZ());
+					Holder.Reference<ConfiguredStructureFeature<?, ?>> targetf = target;
+					ResourceOrTagLocationArgument.Result<ConfiguredStructureFeature<?, ?>> thing = new Result<ConfiguredStructureFeature<?, ?>>() {
+
+						@Override
+						public boolean test(Holder<ConfiguredStructureFeature<?, ?>> t) {
+							return false;
+						}
+
+						@Override
+						public Either<ResourceKey<ConfiguredStructureFeature<?, ?>>, TagKey<ConfiguredStructureFeature<?, ?>>> unwrap() {
+							return Either.left(targetf.key());
+						}
+
+						@Override
+						public <E> Optional<Result<E>> cast(ResourceKey<? extends Registry<E>> p_210997_) {
+							return Optional.empty();
+						}
+
+						@Override
+						public String asPrintable() {
+							return null;
+						}
+					};
+					LocateCommand.showLocateResult(cmdCtx.getSource(), thing, srcpos, dst, "commands.locate.success");
 					return 1;
 				})))
-				/* */
-				/*
-				.then(Commands.literal("goinf").requires(cmdSrc -> {
-					return cmdSrc.hasPermissionLevel(2);
-				}).executes(cmdCtx -> {
-					ServerPlayerEntity p = cmdCtx.getSource().asPlayer();
-					p.setPosition(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-					return 1;
-				}))
-				*/
-				;
+		/* */
+		/*
+		 * .then(Commands.literal("goinf").requires(cmdSrc -> { return
+		 * cmdSrc.hasPermissionLevel(2); }).executes(cmdCtx -> { ServerPlayerEntity p =
+		 * cmdCtx.getSource().asPlayer(); p.setPosition(Double.POSITIVE_INFINITY,
+		 * Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY); return 1; }))
+		 */
+		;
 	}
-	
+
 	private static Map<String, AtomicInteger> structureIdx = new ConcurrentHashMap<>();
 }
